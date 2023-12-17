9a0930ed76d9389bb9a16b18e4586f8a2de21fb8
Astratos <120133182+EmmanuelDevAtx@users.noreply.github.com>
2023-12-17 16:17:23 -0600

    add main data (#6)

--- src/i18n/es.tsx
+++ src/i18n/es.tsx
@@ -5,5 +5,14 @@ export default {
             about:'Acerca me',
             proyects: 'Proyectos'
         }
+    },
+    home:{
+        main_text:{
+            hello:"Hola soy ",
+            name:"Emmanuel Mejia",
+            description1:"Soy un entusiasta del web3, además de desarrollar aplicaciones moviles, web y backend",
+            description2:"Me gusta resolver problkemas y hacer las cosas mas sencillas.",
+            who_am_i:"Ingeniero en Software, Back end y Front end"
+        }
     }
 }
