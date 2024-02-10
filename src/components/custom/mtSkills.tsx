c55147b805d354aa57a794c83cf5c7f736e681e0
Astratos <120133182+EmmanuelDevAtx@users.noreply.github.com>
2024-02-10 17:01:56 -0600

    update animations (#9)

--- src/components/custom/mtSkills.tsx
+++ src/components/custom/mtSkills.tsx
@@ -2,63 +2,92 @@ import Image from 'next/image';
 import Css from '../../styles/myskills.module.css';
 import { Card, Stack, Typography } from '@mui/material';
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { useSettings } from '@/hooks/settingsContext';
 
 type SkillDatatype = {
     url: string,
     text: string;
 }
 const skillsData: SkillDatatype[] = [
-    { url: "/svg/myskills/blockchain.svg", text:"Blockchain" },
-    { url: "/svg/myskills/react.svg", text:"React" },
-    { url: "/svg/myskills/react.svg", text:"React Native" },
-    { url: "/svg/myskills/mongodb.svg", text:"Mongo DB" },
-    { url: "/svg/myskills/next-js.svg", text:"Next js" },
-    { url: "/svg/myskills/nodejs.svg", text:"Node js" },
-    { url: "/svg/myskills/typescript.svg", text:"TypeScript" },
-
-    { url: "/svg/myskills/blockchain.svg", text:"Blockchain" },
-    { url: "/svg/myskills/react.svg", text:"React" },
-    { url: "/svg/myskills/react.svg", text:"React Native" },
-    { url: "/svg/myskills/mongodb.svg", text:"Mongo DB" },
-    { url: "/svg/myskills/next-js.svg", text:"Next js" },
-    { url: "/svg/myskills/nodejs.svg", text:"Node js" },
-    { url: "/svg/myskills/typescript.svg", text:"TypeScript" },
-
-    { url: "/svg/myskills/blockchain.svg", text:"Blockchain" },
-    { url: "/svg/myskills/react.svg", text:"React" },
-    { url: "/svg/myskills/react.svg", text:"React Native" },
+    { url: "/svg/myskills/blockchain.svg", text: "Blockchain" },
+    { url: "/svg/myskills/react.svg", text: "React" },
+    { url: "/svg/myskills/react.svg", text: "React Native" },
+    { url: "/svg/myskills/mongodb.svg", text: "Mongo DB" },
+    { url: "/svg/myskills/next-js.svg", text: "Next js" },
+    { url: "/svg/myskills/nodejs.svg", text: "Node js" },
+    { url: "/svg/myskills/typescript.svg", text: "TypeScript" }
 ]
+skillsData.push(...skillsData);
 
 export const MySkills = () => {
     return (
-        <div className={Css.slider}>
-            <div className={Css.sliderTrack}>
-                {
-                    skillsData.map((item: SkillDatatype, index: number) => {
-                        return <MinCardSkillData 
-                        key={index + item.text} 
-                        url={item.url} 
-                        name={item.text} />
-                    })
-                }
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 4, }}
+        >
+            <div className={Css.slider}>
+                <div className={Css.sliderTrack}>
+                    {
+                        skillsData.map((item: SkillDatatype, index: number) => {
+                            return <MinCardSkillData
+                                id={index + item.url}
+                                key={index + item.text}
+                                url={item.url}
+                                name={item.text} />
+                        })
+                    }
+                </div>
             </div>
-        </div>
+        </motion.div>
     );
 }
 
-const MinCardSkillData = ({ key, url, name }: { key: string, url: string, name: string }) => {
+const MinCardSkillData = ({ id, url, name }: { id:string, url: string, name: string }) => {
+    const [scale, setScale] = useState<number>(0.7);
+      const handleScroll = () => {
+        const element = document.getElementById(id);
+        if (element) {
+            const rect = element.getBoundingClientRect();
+            const elementCenterX = rect.left + rect.width / 2;
+            const windowCenterX = window.innerWidth / 1.7;
+
+            const marginError = 200;
+            if (Math.abs(elementCenterX - windowCenterX) < marginError) {
+                element.classList.add('centered');
+                console.log('centro ')
+                setScale(1);
+            } else {
+                element.classList.remove('centered');
+                console.log('no centro ')
+                setScale(0.7);
+
+            }
+        }
+    };
+
+    useEffect(() => {
+        const intervalo = setInterval(() => {
+          handleScroll();
+        }, 500);
+        return () => {
+          clearInterval(intervalo);
+        };
+      }, []);
+    // FIX: When return scroll, animation get max size too
     return (
         <motion.div
-            key={key}
-            animate={{ scale: [0.7, 1, 0.7] }}
-            //TODO: This need to add infinte loop and 
-            // escale when the icon stay in the center of the screen
-            transition={{ ease: "linear", duration: 4,}}
+            id={id}
+            className={`${Css.slide} ${Css.slider}`} // Agrega las clases necesarias
+            initial={{ scale: 0.7}}
+            animate={{ scale }}
+            transition={{ ease: "easeInOut", duration: 8 }}
         >
-            <Card className={Css.slide} sx={{ borderRadius: 5 , padding:3}}>
+            <Card sx={{ borderRadius: 5, padding: 3 }}>
                 <Stack textAlign={'center'} justifyItems={'center'}>
                     <Image height={100} width={100} src={url} alt={name} />
-                    <Typography> {name}</Typography>
+                    <Typography>{name}</Typography>
                 </Stack>
             </Card>
         </motion.div>
