f27d2a150779ed4bacf5c8167d674a05d1815785
Astratos <120133182+EmmanuelDevAtx@users.noreply.github.com>
2024-02-25 15:05:31 -0600

    update typograpohy

--- src/i18n/es.tsx
+++ src/i18n/es.tsx
@@ -11,7 +11,7 @@ export default {
             hello:"Hola soy ",
             name:"Emmanuel Mejia",
             description1:"Soy un entusiasta del web3, además de desarrollar aplicaciones moviles, web y backend",
-            description2:"Me gusta resolver problkemas y hacer las cosas mas sencillas.",
+            description2:"Me gusta resolver problemas y hacer las cosas mas sencillas.",
             who_am_i:"Ingeniero en Software, Back end y Front end"
         }
     },
