4cd210d3f5c7fc5aefd938d555687a9b55cd10f4
Astratos <120133182+EmmanuelDevAtx@users.noreply.github.com>
2024-03-18 14:28:00 -0600

    adding needed information

--- src/components/proyects/selectProyect.tsx
+++ src/components/proyects/selectProyect.tsx
@@ -5,6 +5,10 @@ import { useEffect, useState } from "react"
 import proyectsCss from '../../styles/proyects.module.css';
 import { ButtonItemProyect, ItemProyect } from "../custom/itemProyect";
 import { motion } from "framer-motion";
+import { ButtonSimple } from "../custom/buttonSimple";
+import WebIcon from '/public/svg/tecnnologies/web_icon.svg';
+import GitHub from '/public/svg/redirecIcons/github.svg';
+
 
 export const SelectProyects = ({ data, tabSelected, isChangeTab }: { data: ItemProyectInputType[], tabSelected: MyProyectsTabEnum, isChangeTab: boolean }) => {
 
@@ -42,6 +46,10 @@ export const SelectProyects = ({ data, tabSelected, isChangeTab }: { data: ItemP
         }, 800);
     }
 
+    function RedirectLink(url: string) {
+        window.open(url);
+    };
+
     return (
         <Grid container>
             <Grid item md={3}>
@@ -62,13 +70,13 @@ export const SelectProyects = ({ data, tabSelected, isChangeTab }: { data: ItemP
                             }
                         </List>
 
-                        : <Tabs variant="scrollable" scrollButtons="auto" sx={{ width : width *0.8, overflow: 'auto', marginBottom: 2,  backdropFilter: 'blur(10px)', backgroundColor: `rgba(${isDarkMode ? 255 : 0},${isDarkMode ? 255 : 0},${isDarkMode ? 255 : 0},${isDarkMode ? 0.05 : 0.1})`, borderRadius: 4 }}>
+                        : <Tabs variant="scrollable" scrollButtons="auto" sx={{ width: width * 0.8, overflow: 'auto', marginBottom: 2, backdropFilter: 'blur(10px)', backgroundColor: `rgba(${isDarkMode ? 255 : 0},${isDarkMode ? 255 : 0},${isDarkMode ? 255 : 0},${isDarkMode ? 0.05 : 0.1})`, borderRadius: 4, border: `1px ${theme.palette.primary.main} solid` }}>
                             {
                                 arrayFilterData.map((item: ItemProyectInputType, index: number) => {
                                     return (
                                         <Tab key={`arrayFilterData${item.title}${index}`} onClick={() => changeData(item)} sx={{ marginY: 1, borderRadius: 3, transition: '0.7s ease', backgroundColor: selectedData?.title === item.title ? `rgba(${darkOrLigth}, ${darkOrLigth}, ${darkOrLigth}, 0.1)` : 'transparent', color: selectedData?.title === item.title ? theme.palette.primary.main : theme.palette.text.primary }} label={item.title}
                                         />
-                                    
+
                                     );
                                 })
                             }
@@ -78,53 +86,69 @@ export const SelectProyects = ({ data, tabSelected, isChangeTab }: { data: ItemP
             </Grid>
             <Grid item md={9} paddingLeft={2} sx={{ height: '300px' }}>
                 <motion.div
-                    style={{ height: '300px', backgroundColor: theme.palette.background.default, borderRadius: '5px', backdropFilter: 'blur(5px)' }}
+                    style={{ height: '300px', backgroundColor: theme.palette.background.default, borderRadius: '5px' }}
                     animate={{ x: valueMoveCards, opacity: cardOpacity, transition: { duration: durationTransition } }}
                     initial={{ x: 100, opacity: 0 }}>
 
-                        {!isSmallScreen ? 
-                    <Grid container paddingX={2} paddingY={3} sx={{ height: '100%', border: '1px rgb(60, 60, 60) solid', borderRadius: 5 }}>
-                        <Grid item md={8}>
-                            <Stack justifyContent={'space-between'} sx={{ width: '100%', height: '100%' }}>
-                                <Typography variant="h5" color="primary">
-                                    {selectedData?.title}
-                                </Typography>
-                                <Typography color={theme.palette.text.disabled}>
-                                    {selectedData?.subtitle}
-                                </Typography>
-                                <Typography variant="body2" sx={{ marginY: 1.5 }}>
-                                    {isSpanish ? selectedData?.description_es : selectedData?.description_en}
-                                </Typography>
-                                {
-                                    selectedData?.technology &&
-                                    <Stack direction={'row'} sx={{ width: '100%', overflow: 'auto' }}>
-                                        {selectedData?.technology.map((item: Technologies, index: number) => {
-                                            return (
-                                                <Box key={'ItemProyectTechnologies' + index} sx={{ height: 55, width: 50 }} >
-                                                    <ButtonItemProyect
-                                                        styleMotionDiv={{ backgroundColor: isDarkMode ? theme.palette.text.secondary : 'rgba(0,0,0,0.8)' }}
-                                                        iconsEnum={item.iconSelected} />
-                                                </Box>
-                                            )
-                                        })}
+                    {!isSmallScreen ?
+                        <Grid container paddingX={2} paddingY={3} sx={{ height: '100%', border: '1px rgb(60, 60, 60) solid', borderRadius: 5 }}>
+                            <Grid item md={8}>
+                                <Stack justifyContent={'space-between'} sx={{ width: '100%', height: '100%' }}>
+                                    <Stack direction={'row'} justifyContent={'space-between'}>
+
+                                        <Typography variant="h5" color="primary" width={'80%'}>
+                                            {selectedData?.title}
+                                        </Typography>
+                                        <Stack direction={'row'} width={'20%'}>
+                                        {selectedData?.gitHubRepositoryLink &&
+                                        <ButtonSimple
+                                            styleMotionDiv={{ flex: 1, padding: 4, height: 40, width: 40, borderRadius: 20, paddingInline: 5.5, border: ` 1px ${theme.palette.cardProyect.main} solid` }}
+                                            onClick={() => RedirectLink(selectedData?.gitHubRepositoryLink ?? '')}
+                                        ><GitHub style={{ fill: theme.palette.cardProyect.main, height: '100%', width: '100%' }} /></ButtonSimple>}
+                                        {selectedData?.linkUrlProyect &&
+                                        <ButtonSimple
+                                            styleMotionDiv={{ flex: 1, padding: 4, height: 40, width: 40, borderRadius: 20, paddingInline: 5.5, border: ` 1px ${isDarkMode ? theme.palette.primary.main : theme.palette.cardProyect.main} solid` }}
+                                            onClick={() => RedirectLink(selectedData?.linkUrlProyect ?? '')}
+                                        ><WebIcon style={{ fill: isDarkMode ? theme.palette.primary.main : theme.palette.cardProyect.main, height: '100%', width: '100%' }} /></ButtonSimple>}
+                                        </Stack>
+                                        
                                     </Stack>
-                                }
-                            </Stack>
+                                    <Typography color={theme.palette.text.disabled}>
+                                        {selectedData?.subtitle}
+                                    </Typography>
+                                    <Typography variant="body2" sx={{ marginY: 1.5 }}>
+                                        {isSpanish ? selectedData?.description_es : selectedData?.description_en}
+                                    </Typography>
+                                    {
+                                        selectedData?.technology &&
+                                        <Stack direction={'row'} sx={{ width: '100%', overflow: 'auto' }}>
+                                            {selectedData?.technology.map((item: Technologies, index: number) => {
+                                                return (
+                                                    <Box key={'ItemProyectTechnologies' + index} sx={{ height: 55, width: 50 }} >
+                                                        <ButtonItemProyect
+                                                            styleMotionDiv={{ backgroundColor: isDarkMode ? theme.palette.text.secondary : 'rgba(0,0,0,0.8)' }}
+                                                            iconsEnum={item.iconSelected} />
+                                                    </Box>
+                                                )
+                                            })}
+                                        </Stack>
+                                    }
+                                </Stack>
+                            </Grid>
+                            <Grid item md={4}>
+                                <Card>
+                                    {selectedData &&
+                                        <CardMedia
+                                            component="img"
+                                            image={selectedData.imageUrl}>
+                                        </CardMedia>
+                                    }
+                                </Card>
+                            </Grid>
                         </Grid>
-                        <Grid item md={4}>
-                            <Card>
-                                {selectedData &&
-                                    <CardMedia
-                                        component="img"
-                                        image={selectedData.imageUrl}>
-                                    </CardMedia>
-                                }
-                            </Card>
-                        </Grid>
-                    </Grid>
-                        
-                        
-                        : <ItemProyect subtitle={selectedData?.subtitle} linkUrlProyect={selectedData?.linkUrlProyect} imageUrl={selectedData?.imageUrl} title={selectedData?.title} technology={selectedData?.technology} gitHubRepositoryLink={selectedData?.gitHubRepositoryLink}/>}
+
+
+                        : <ItemProyect subtitle={selectedData?.subtitle} linkUrlProyect={selectedData?.linkUrlProyect} imageUrl={selectedData?.imageUrl} title={selectedData?.title} technology={selectedData?.technology} gitHubRepositoryLink={selectedData?.gitHubRepositoryLink} />}
                 </motion.div>
 
             </Grid>
